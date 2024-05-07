3993e4278410f29ab0fb8bb532589528c07efc70
Pranay <pranay061998@gmail.com>
2024-05-08 00:14:52 +0530

    Ghost Batch Normalization

---
+++ TabNet/GBN.py
@@ -0,0 +1,20 @@
+# GHOST Batch Normalization
+import torch
+import torch.nn
+from torch.nn import BatchNorm1d
+
+class ghost_BN(torch.nn.Module):
+
+    def __init__(self, inp_dim, virtual_batch_size = 128, momentum = 0.2, epsilon = 0.1):
+        super(ghost_BN, self).__init__()
+        self.virtual_batch_size = virtual_batch_size
+        self.bn = BatchNorm1d(inp_dim, momentum=momentum, eps=epsilon)
+
+    def forward(self,x):
+        chunk_size= int(np.ceil(x.shape[0]/self.virtual_batch_size))
+        chunks = x.chunk(chunk_size, dim=0)
+        batch_list = [self.bn(chunk) for chunk in chunks]
+        return torch.cat(batch_list, dim=0)
+
+
+pp = ghost_BN(2)
